62805112d6a7330689a46755c7d2eb1200b2902c
maxson.carvalho <maxson.carvalho@findes.org.br>
2021-07-13 20:51:23 -0300

    First Commit

---
+++ ApiMax/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiMax.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiMax.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Logar")]
+        public IActionResult Logar(LoginViewModelInput LoginViewModelInput)
+        {
+            return Ok(LoginViewModelInput);
+        } 
+        
+        [HttpPost]
+        [Route("Registrar")]
+        public IActionResult Registrar(RegistroViewModelInput RegistroViewModelInput)
+        {
+            return Created("", RegistroViewModelInput);
+        }
+    }
+}
---
+++ ApiMax/Models/Usuarios/RegistroViewModelInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiMax.Models.Usuarios
+{
+    public class RegistroViewModelInput
+    {
+        [Required(ErrorMessage = "O login é obrigatório!")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "O E-mail é obrigatório!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O login é obrigatório!")]
+        public string Senha { get; set; }
+    }
+}
